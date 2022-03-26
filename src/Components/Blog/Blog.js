8803b8f2aba9742f9e0516669594e1e52b5285f0
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-03-26 22:30:28 +0600

    only because my laptop was crashed

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -6,19 +6,11 @@ const Blog = () => {
         <div className='blog-container'>
             <div>
                 <h3>How React Works?</h3>
-                <p>Ra JavaScript library that uses declarative programming to construct efficient user interfaces.
-                    Using React, we can create single-page applications and mobile apps.
-                    React uses virtual DOM, which is a JavaScript version of a DOM tree.
-                    As a result, it will use the virtual representation of the DOM when reading or writing to it.
-                    The virtual DOM will then attempt to update the browser's DOM in the most efficient manner possible.</p>
+                <p></p>
             </div>
             <div>
-                <h3>Props Vs State</h3>
-                <p>Props are used to pass data from one component to onother component.
-                    On the other hand state is a local data storage, this is local component only.
-                    It's can not be pass any component any where. if user interfare website, they can be change anything
-                    in website interface it is called state. If user inteface in the ui state can be
-                    change but props could not be change.
+                <h3>Difference between Props and State</h3>
+                <p>
                 </p>
             </div>
         </div>
